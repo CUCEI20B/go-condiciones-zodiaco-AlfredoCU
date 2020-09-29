daf138a3fa650689ab288511a9b7427f3b9b31b2
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-29 13:20:42 -0500

    Programa para pedir al usuario el día y mes de nacimiento, y de acuerdo a su día y mes de nacimiento, mostrar en pantalla su signo zodiacal.

--- main.go
+++ main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	// Declaration of variables
+	var (
+		day = 0
+		month = 0
+		zodiac = ""
+	)
+
+	// Input data
+	fmt.Scan(&day)
+	fmt.Scan(&month)
+
+	// Conditions
+	if  day >= 21 && month == 1 || day <= 18 && month == 2 {
+		zodiac = "acuario"
+	} else if day >= 19 && month == 2 || day <= 20 && month == 3 {
+		zodiac = "piscis"
+	} else if day >= 21 && month == 3 || day <= 20 && month == 4 {
+		zodiac = "aries"
+	} else if day >= 21 && month == 4 || day <= 20 && month == 5 {
+		zodiac = "tauro"
+	} else if day >= 21 && month == 5 || day <= 21 && month == 6 {
+		zodiac = "geminis"
+	} else if day >= 22 && month == 6 || day <= 22 && month == 7 {
+		zodiac = "cancer"
+	} else if day >= 23 && month == 7 || day <= 22 && month == 8 {
+		zodiac = "leo"
+	} else if day >= 23 && month == 8 || day <= 22 && month == 9 {
+		zodiac = "virgo"
+	} else if day >= 23 && month == 9 || day <= 22 && month == 10 {
+		zodiac = "libra"
+	} else if day >= 23 && month == 10 || day <= 22 && month == 11 {
+		zodiac = "escorpio"
+	} else if day >= 23 && month == 11 || day <= 21 && month == 12 {
+		zodiac = "sagitario"
+	} else { // day >= 22 && month == 12 || day <= 20 && month == 1
+		zodiac = "capricornio"
+	}
+
+	// Print result
+	println(zodiac)
 }
